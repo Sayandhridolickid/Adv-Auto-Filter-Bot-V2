4f6c1d554089f28e481edad61639fe4d73f8178c
Wade Wilson <84837136+Sayandhridolickid@users.noreply.github.com>
2021-06-08 16:58:49 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -98,7 +98,15 @@ async def start(bot, update):
     ]]
     
     reply_markup = InlineKeyboardMarkup(buttons)
-    
+    await bot.send_photo(
+        chat_id=update.chat.id,
+        photo="https://telegra.ph/file/f1a0f56364e433ab72dc0.jpg",
+        caption=Translation.START_TEXT.format(
+                update.from_user.first_name),
+        reply_markup=reply_markup,
+        parse_mode="html",
+        reply_to_message_id=update.message_id
+    )
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT.format(
